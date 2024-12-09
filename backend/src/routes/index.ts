36124867d4c4844d75531cf6843085e97b45ecaa
Ivan Petrovic <ip@ivan-petrovic.de>
2024-12-09 19:12:35 +0100

    register endpoint added

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
 
+import registerRoutes from './register';
+
 const router = express.Router();
 
 // Root endpoint
 router.get("/", (_req: Request, res: Response) => {
   res.send("Node.js Server is running!");
 });
+
+router.use("/register", registerRoutes);
 
 export default router
